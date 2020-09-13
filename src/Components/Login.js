a688ab50ea4e9385ab4aa9f683399b2ec2478d09
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-12 22:38:13 -0400

    checking history.push

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,7 @@ export default function Login(props) {
     return (
         <div>
             <h1>I am from login</h1>
+            <button onClick = {() =>props.history.push("/register")}>Go to register</button>
         </div>
     )
 }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Register() {
+export default function Register(props) {
     return (
         <div>
             <h1>I am from Register</h1>
+            <button onClick = {() =>props.history.push("/login")}>Go to login</button>
         </div>
     )
 }
