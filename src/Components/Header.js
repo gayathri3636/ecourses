fddf3c5ec6c335e287c77a4666a354a0d740f432
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-20 18:19:40 -0400

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './Header.scss'
+import { Link } from 'react-router-dom';
+import './Header.scss';
 
 export default function Header() {
-    return (
-        <div className = "header">
-            <div className = "login">
-                <Link className = "catalog" to = "/login">Login</Link>
-                <Link to = "/register">Register</Link>
-                <Link className = "contact" to = "/contact">Contact</Link> 
-            </div>
-            
-            <div className = "course">
-                <Link className = "addcourse" to="/Addcourse">AddCourse</Link>
-                <Link  to = "/catalog">Catalog</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className='header'>
+      <div className='login'>
+        <Link className='catalog' to='/login'>
+          Login
+        </Link>
+        <Link to='/register'>Register</Link>
+        <Link className='contact' to='/contact'>
+          Contact
+        </Link>
+      </div>
+
+      <div className='course'>
+        <Link className='addcourse' to='/Addcourse'>
+          AddCourse
+        </Link>
+        <Link to='/catalog'>Catalog</Link>
+      </div>
+    </div>
+  );
 }
